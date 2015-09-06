0071ba8f16311baf12436bb743b0afbede15b160
Brian Refsdal <brian.refsdal@gmail.com>
2015-09-06 10:44:35 -0700

    Add logic

--- ZeromqTest/publisher.c
+++ ZeromqTest/publisher.c
@@ -8,9 +8,41 @@
 
 #include <stdio.h>
 #include "zmq.h"
+#include "zhelpers.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    char hostname_and_port[256], time_buf[26], message_buf[36];
+    void *ctx = NULL, *publisher = NULL;
+    int retval = 0, lucky_number;
+    time_t current_time;
+    
+    if (argc < 3) {
+        printf("Usage: %s host port", argv[0]);
+        exit(1);
+    }
+    
+    sprintf(hostname_and_port, "tcp://%s:%s", argv[1], argv[2]);
+    
+    ctx = zmq_ctx_new();
+    publisher = zmq_socket(ctx, ZMQ_PUB);
+    retval = zmq_bind(publisher, hostname_and_port);
+    
+    if (retval != 0) {
+        fprintf(stderr, "Unable to bind zmq socket on host %s port %s\n", argv[1], argv[2]);
+        exit(2);
+    }
+    
+    srandom(42);
+    while(1) {
+        current_time = time(NULL);
+        lucky_number = randof (1000000);
+        ctime_r(&current_time, time_buf);
+        sprintf(message_buf, "%d %s", lucky_number, time_buf);
+        s_send(publisher, message_buf);
+    }
+    
+    zmq_close(publisher);
+    zmq_ctx_destroy(ctx);
     return 0;
 }
--- ZeromqTest/subscriber.c
+++ ZeromqTest/subscriber.c
@@ -7,10 +7,41 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "zmq.h"
+#include "zhelpers.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    char hostname_and_port[256];
+    void *ctx = NULL, *subscriber = NULL;
+    int retval = 0, ii = 0;
+    
+    if (argc < 3) {
+        printf("Usage: %s host port", argv[0]);
+        exit(1);
+    }
+    
+    sprintf(hostname_and_port, "tcp://%s:%s", argv[1], argv[2]);
+    
+    ctx = zmq_ctx_new();
+    subscriber = zmq_socket(ctx, ZMQ_SUB);
+    retval = zmq_bind(subscriber, hostname_and_port);
+    
+    if (retval != 0) {
+        fprintf(stderr, "Unable to connect to zmq socket on host %s port %s\n", argv[1], argv[2]);
+        exit(2);
+    }
+    
+    retval = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, NULL, 0);
+    
+    for (ii = 0; ii < 100; ii++) {
+        char *message = (char *) s_recv(subscriber);
+        printf("[%d]: '%s'\n", ii+1, message);
+        free(message);
+    }
+    
+    zmq_close(subscriber);
+    zmq_ctx_destroy(ctx);
     return 0;
 }
