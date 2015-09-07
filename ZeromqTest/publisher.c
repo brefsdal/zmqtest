d58755cfb56c5733b2798fd7839d76a4f3e2e84c
Brian Refsdal <brian.refsdal@gmail.com>
2015-09-06 19:08:12 -0700

    Add scala

--- ZeromqTest/publisher.c
+++ ZeromqTest/publisher.c
@@ -7,14 +7,17 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "zmq.h"
-#include "zhelpers.h"
+//#include "zhelpers.h"
 
 int main(int argc, const char * argv[]) {
 
     char hostname_and_port[256], time_buf[26], message_buf[36];
     void *ctx = NULL, *publisher = NULL;
-    int retval = 0, lucky_number;
+    int retval = 0, nbytes;
+    long lucky_number;
     time_t current_time;
     
     if (argc < 3) {
@@ -22,10 +25,11 @@ int main(int argc, const char * argv[]) {
         exit(1);
     }
     
-    sprintf(hostname_and_port, "tcp://%s:%s", argv[1], argv[2]);
+    snprintf(hostname_and_port, 256, "tcp://%s:%s", argv[1], argv[2]);
     
     ctx = zmq_ctx_new();
     publisher = zmq_socket(ctx, ZMQ_PUB);
+    printf("binding to %s", hostname_and_port);
     retval = zmq_bind(publisher, hostname_and_port);
     
     if (retval != 0) {
@@ -36,10 +40,14 @@ int main(int argc, const char * argv[]) {
     srandom(42);
     while(1) {
         current_time = time(NULL);
-        lucky_number = randof (1000000);
+        lucky_number = rand();
         ctime_r(&current_time, time_buf);
-        sprintf(message_buf, "%d %s", lucky_number, time_buf);
-        s_send(publisher, message_buf);
+        time_buf[24] = '\0';
+        snprintf(message_buf, 36, "%s %ld", time_buf, lucky_number % 1000000L);
+        nbytes = zmq_send(publisher, message_buf, 36, 0);
+        if (nbytes != 36) {
+            fprintf(stderr, "ERROR: Unable to send 36 bytes to host %s port %s\n", argv[1], argv[2]);
+        }
     }
     
     zmq_close(publisher);
--- ZeromqTest/subscriber.c
+++ ZeromqTest/subscriber.c
@@ -9,36 +9,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "zmq.h"
-#include "zhelpers.h"
 
 int main(int argc, const char * argv[]) {
     
-    char hostname_and_port[256];
+    char hostname_and_port[256], message_buf[36];
     void *ctx = NULL, *subscriber = NULL;
-    int retval = 0, ii = 0;
+    int retval = 0, ii = 0, nbytes = 0;
     
     if (argc < 3) {
         printf("Usage: %s host port", argv[0]);
         exit(1);
     }
     
-    sprintf(hostname_and_port, "tcp://%s:%s", argv[1], argv[2]);
+    snprintf(hostname_and_port, 256, "tcp://%s:%s", argv[1], argv[2]);
     
     ctx = zmq_ctx_new();
     subscriber = zmq_socket(ctx, ZMQ_SUB);
-    retval = zmq_bind(subscriber, hostname_and_port);
+    printf("connecting to %s", hostname_and_port);
+    retval = zmq_connect(subscriber, hostname_and_port);
     
     if (retval != 0) {
-        fprintf(stderr, "Unable to connect to zmq socket on host %s port %s\n", argv[1], argv[2]);
+        fprintf(stderr, "ERROR: Unable to connect to zmq socket on host %s port %s\n", argv[1], argv[2]);
         exit(2);
     }
     
     retval = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, NULL, 0);
     
     for (ii = 0; ii < 100; ii++) {
-        char *message = (char *) s_recv(subscriber);
-        printf("[%d]: '%s'\n", ii+1, message);
-        free(message);
+        nbytes = zmq_recv(subscriber, message_buf, 36, 0);
+        if (-1 == nbytes) {
+            fprintf(stderr, "ERROR: Unable to read bytes on host %s port %s\n", argv[1], argv[2]);
+        }
+        printf("%s\n", message_buf);
     }
     
     zmq_close(subscriber);
